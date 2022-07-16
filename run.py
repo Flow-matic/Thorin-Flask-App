51a6e9bd32898df3255ce6c7be9fb703b0d5ab38
Flow-matic <jonathonvinson22@gmail.com>
2022-07-16 19:31:17 +0000

    Added secret key comment to run.py file

--- run.py
+++ run.py
@@ -51,3 +51,8 @@ if __name__ == "__main__":
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
         debug=True)
+
+
+#import os
+
+#os.environ.setdefault("SECRET_KEY", "your_secret_key_here")
